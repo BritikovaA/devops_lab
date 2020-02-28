72af59e13736590112f46dc12ce6041376ee98ef
student <student@EPBYMINW9139.minsk.epam.com>
2020-02-28 12:40:28 +0300

    commit

--- hometask1.sh
+++ hometask1.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
+sudo yum install python-pip
 sudo yum -y install git gcc zlib-devel bzip2-devel readline-devel sqlite-devel openssl-devel
 
 curl -L  https://raw.github.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
 
-$(pyenv init -) ="$(pyenv init -)" 
-
 cat <<EOF >> ~/.bashrc
-export PATH="/home/$USERNAME/.pyenv/bin:$PATH"
+export PATH="/home/$USER/.pyenv/bin:\$PATH"
 eval "\$(pyenv init -)" 
 eval "\$(pyenv virtualenv-init -)"
 EOF
 
 source ~/.bashrc
 
-pyenv install 2.7.5
-pyenv install 3.7.5
+pyenv install 2.7.16
+pyenv install 3.7.0
 
 sudo pip install virtualenv
 
+pyenv virtualenv 2.7.16 environment_2.7
 pyenv virtualenv 3.7.0 environment_3.7
-pyenv virtualenv 2.7.0 environment_2.7
 
