92197bff281017c7a4c09ed1741e268321800a0f
BritikovaA <britikova.a@gmail.com>
2020-03-03 19:42:47 +0300

    New commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="snapshot",
+    packages=find_packages(),
+    scripts=["snapshot"],
+    version="1.0",
+    author="Anastasiya Brytsikava",
+    author_email="Anastasiya_Brytsikava@epam.com",
+    description="Application for taking snapshots"
+)
