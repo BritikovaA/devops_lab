1a1f95bbc79009d51113486d9ffbe29f28061f83
BritikovaA <britikova.a@gmail.com>
2020-03-03 13:45:10 +0300

    Last(I believe) commit

--- task4.py
+++ task4.py
@@ -6,11 +6,11 @@ if num >= 0 and num <= 9:
     print(num)
 digits = []
 for i in range(9, 1, -1):
-    while (num % i == 0):
+    while num % i == 0:
         digits.append(i)
         num = num // i
 k = 0
-while (len(digits) != 0):
+while digits:
     k = k * 10 + digits[-1]
     digits.pop()
 if k == 0:
